32d99db435da440d19af20e86bca38871cece531
emilbergs <emilberg@outlook.dk>
2020-03-19 12:11:13 +0100

    16.commit(newoutput)

--- script/script.js
+++ script/script.js
@@ -1,16 +1,16 @@
-const getCurrentTimes = document.querySelector("knap-1");
+const getTextBtnElement = document.getElementById("getCurrentTimes");
 
-getCurrentTimes.addEventListener("click", knap);
+getTextBtnElement.addEventListener("click", getCurrentTimes);
 
-function knap(){
+function getCurrentTimes(){
     fetch('https://api.myjson.com/bins/1dgp04')
     .then((res) => res.json())
     .then((data) => {
         let output = '<h2 class="display-4 mb-4>bins</h2>';
-        data.forEach(function(tider){
+        data.forEach(function(user){
             output+= `
 <ul class="list-group mb-3">
-<li class="list-group-item">Tid: ${tider.Time}</li>
+<li class="list-group-item">Tid: ${bins.Time}</li>
 </ul>
 `;
         })
